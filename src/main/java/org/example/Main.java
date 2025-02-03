62058974dad29b4810a423bb9039cbff8fdba5bb
Eddy-Henne <eduardhenne@hotmail.de>
2025-02-03 14:41:39 +0100

    add_digit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        String password = "pAs5w0rd";
+        //Passwort-Eingabe und erfolgreich verifiziert
+        String password = "oord";
         if(isPasswordValid(password)) {
             System.out.println("Ihr gewähltes Passwort ist sicher!");
         }
@@ -11,16 +12,27 @@ public class Main {
 
     }
 
+        //Rückgabe-Werte zusammengefasste Überprüfung & Fehlerausgabe
     public static boolean isPasswordValid(String password) {
         if(!isPasswordLengthValid(password))
-            System.out.println("ihr Passwort hat weniger als 8 Zeichen");
+            System.out.println("Ihr Passwort ist unsicher; verwenden Sie mindestens 8 Zeichen!");
+        if (!containsDigits(password))
+            System.out.println("Ihr Passwort ist unsicher; verwenden Sie mindestens eine Zahl");
 
-        return isPasswordLengthValid(password);
+        return isPasswordLengthValid(password) && containsDigits(password);
     }
 
-
+        //Überprüfung mindestens 8 Zeichen
     public static boolean isPasswordLengthValid(String password) {
         return password.length() >= 8;
     }
 
+        //Überprüfung mindestens eine Zahl
+    public static boolean containsDigits(String password) {
+        for(char character : password.toCharArray()) {
+            if (Character.isDigit(character))
+                return true;
+        }
+        return false;
+    }
 }
