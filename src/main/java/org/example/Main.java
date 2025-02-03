d42de46c1fc0a9ac87270319f1c1a8693895e8b2
Eddy-Henne <eduardhenne@hotmail.de>
2025-02-03 14:18:56 +0100

    add_PasswordLength

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,25 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        String password = "pAs5w0rd";
+        if(isPasswordValid(password)) {
+            System.out.println("Ihr gewähltes Passwort ist sicher!");
+        }
+
+
     }
+
+    public static boolean isPasswordValid(String password) {
+        if(!isPasswordLengthValid(password))
+            System.out.println("ihr Passwort hat weniger als 8 Zeichen");
+
+        return isPasswordLengthValid(password);
+    }
+
+
+    public static boolean isPasswordLengthValid(String password) {
+        return password.length() >= 8;
+    }
+
 }
